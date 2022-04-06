aecf05e3ab3b170e9fa618e51ca409131d3dd11a
gerald0mc <73139664+gerald0mc@users.noreply.github.com>
2022-04-05 22:10:35 -0500

    Delete src/main/dallas directory

--- src/main/dallas/mod/mods/client/HUDEditor.java
+++
@@ -1,9 +0,0 @@
-package me.gerald.dallas.mod.mods.client;
-
-import me.gerald.dallas.mod.Module;
-
-public class HUDEditor extends Module {
-    public HUDEditor() {
-        super("HUDEditor", Category.CLIENT, "");
-    }
-}
