dd811f978e08d701e85202ca555690441da4b42a
bush-did-711 <39170869+bush-did-711@users.noreply.github.com>
2022-04-22 16:40:31 -0700

    gradl

--- src/main/java/me/gerald/dallas/features/module/hud/packetlog/PacketLog.java
+++ src/main/java/me/gerald/dallas/features/module/hud/packetlog/PacketLog.java
@@ -1,40 +1,41 @@
 package me.gerald.dallas.features.module.hud.packetlog;
 
 import me.gerald.dallas.event.events.PacketEvent;
-import me.gerald.dallas.features.gui.comps.settingcomps.BooleanComponent;
 import me.gerald.dallas.features.module.hud.HUDModule;
 import me.gerald.dallas.setting.settings.BooleanSetting;
 import me.gerald.dallas.utils.ReflectionUtil;
 import net.minecraft.network.Packet;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+import static me.gerald.dallas.utils.ReflectionUtil.REFLECTIONS;
+
 public class PacketLog extends HUDModule {
-    public static List<BooleanSetting> settings = new ArrayList<>();
-    public static Map<String, Class<Packet<?>>> packets = new HashMap<>();
+
+    @SuppressWarnings("rawtypes") // either too high or java just sucks more than i remember :kotlin:
+    public static Map<Class<? extends Packet>, BooleanSetting> packets = new HashMap<>();
 
     public PacketLog() {
         super(new PacketLogComponent(1, 11, 1, 1), "PacketLog", Category.HUD, "Shows the players ping.");
-        ReflectionUtil.getSubclasses(Packet.class).forEach(packet -> {
-            String full = packet.getName();
-            String pkg = packet.getPackage().getName();
-            String name = Stringu
-            packets.pu
-            BooleanSetting setting = new BooleanSetting(, false);
-        });
+        REFLECTIONS.getSubTypesOf(Packet.class).forEach(packet ->
+                packets.put(packet, register(new BooleanSetting(ReflectionUtil.betterSimpleName(packet), false)))
+        );
     }
 
     @SubscribeEvent
     public void onPacketSend(PacketEvent.Send event) {
-
+        handlePacket(event.getPacket());
     }
 
     @SubscribeEvent
     public void onPacketReceive(PacketEvent.Receive event) {
+        handlePacket(event.getPacket());
+    }
+
+    private void handlePacket(Packet<?> packet) {
+        if (!packets.get(packet.getClass()).getValue()) return;
 
     }
 }
--- src/main/java/me/gerald/dallas/utils/ReflectionUtil.java
+++ src/main/java/me/gerald/dallas/utils/ReflectionUtil.java
@@ -1,5 +1,7 @@
 package me.gerald.dallas.utils;
 
+import net.minecraft.network.Packet;
+import org.apache.commons.lang3.StringUtils;
 import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
 import org.reflections.util.ConfigurationBuilder;
@@ -10,11 +12,11 @@ import java.util.Set;
  * @since 4/22/2022
  */
 public class ReflectionUtil {
-    private static final Reflections REFLECTIONS = new Reflections(new ConfigurationBuilder().setScanners(new SubTypesScanner()));
+    public static final Reflections REFLECTIONS = new Reflections(new ConfigurationBuilder().setScanners(new SubTypesScanner()));
 
-    public static <T> Set<Class<? extends T>> getSubclasses(Class<T> clazz) {
-        return REFLECTIONS.getSubTypesOf(clazz);
+    public static String betterSimpleName(Class<?> clazz) {
+        String full = clazz.getName();
+        String pckg = clazz.getPackage().getName();
+        return StringUtils.substringAfter(full, pckg + ".");
     }
-
-    //public static String si
 }
