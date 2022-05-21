ef8897b144a8bad48a07da95164461c042c0a5e8
Sxmurai <gavrw0607@gmail.com>
2022-05-20 22:47:54 -0500

    i love lean (i dont)

--- src/main/java/me/gerald/dallas/features/modules/hud/speed/Speed.java
+++ src/main/java/me/gerald/dallas/features/modules/hud/speed/Speed.java
@@ -4,6 +4,6 @@ import me.gerald.dallas.features.modules.hud.HUDModule;
 
 public class Speed extends HUDModule {
     public Speed() {
-        super(new SpeedComponent(1, 181, 1, 1), "Speed", Category.HUD, "Renders all enabled modules.");
+        super(new SpeedComponent(1, 181, 1, 1), "SpeedView", Category.HUD, "Renders all enabled modules.");
     }
 }
